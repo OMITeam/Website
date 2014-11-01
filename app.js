55d7b2dfd9b7343d79b009f85835ff32759bb1c0
Ofir Pernik <ofirp74@gmail.com>
2014-11-01 22:50:55 +0200

    update

--- app.js
+++ app.js
@@ -14,19 +14,12 @@ main = function () {
     $(".dropdown-menu").click(function () { /*dropdown hiding*/
         $(".dropdown-menu").hide();
     });
-    $(".dropdown-toggle").click(function () {
-        $(".dropdown-menu").toggle();
-    });
-    $(".dropdown-menu").click(function () {
-        $(".dropdown-menu").hide();
-    });
     //Collapses the navigation bar when scrolled down
     $(document).scroll(function () {
         var current_scroll_top = $(document).scrollTop();
         if (current_scroll_top >= 50) {
             $(".transperent_nav").removeClass("transperent_nav" , 200);
-        }
-        else {
+        } else {
             $("#Main_nav").addClass("transperent_nav" , 200);
         }
     });
