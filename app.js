19febf1f229064013f7e5366218c2440b95d1974
BomB1La <ofirp74@gmail.com>
2014-11-02 13:28:24 +0200

    FCK UPDATE

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 main = function () {
     $(".navbar-nav li").click(function () {
-        var dropdown = $(".dropdown");
-        if (dropdown.hasClass("activate")) { /*dropdown checking*/
-            $(".dropdown-menu").hide(300);
-        }
         var current = $(".activate");
-        current.removeClass("activate");
-        $(this).addClass("activate");
+        if(current !== null){
+            current.removeClass("activate");
+        }
+        if(!$(this).hasClass("dropdown")){
+            $(this).addClass("activate");
+        }
     });
 
     $(".dropdown-toggle").click(function () { /*dropdown toggling*/
@@ -19,7 +19,7 @@ main = function () {
     //Collapses the navigation bar when scrolled down
     $(document).scroll(function () {
         var current_scroll_top = $(document).scrollTop();
-        if (current_scroll_top >= 50) {
+        if (current_scroll_top >= 30) {
             $(".transperent_nav").removeClass("transperent_nav" , 200);
         } else {
             $("#Main_nav").addClass("transperent_nav" , 200);
