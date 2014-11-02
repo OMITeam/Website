5c05c329d6230fa8262b6edfd0d0342e67781d38
Michael Brickman <mbricman8@gmail.com>
2014-11-02 15:33:40 +0200

    Changing the readability
    Changing the readability  and commenting on the CSS HTML & JS files

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 main = function () {
-    $(".navbar-nav li").click(function () {
-        var current = $(".activate");
-        if(current !== null){
-            current.removeClass("activate", 600);
-        }
-        if(!$(this).hasClass("dropdown")){
-            $(this).addClass("activate", 600);
-        }
-    });
+    /*Navigation bar*/
+        $(".navbar-nav li").click(Navnar_highlight);
+        /*dropdown menu*/
+            $(".dropdown-toggle").click(function () {
+                $(".dropdown-menu").toggle(600);
+            });
+            $(".dropdown-menu").click(function () {
+                $(".dropdown-menu").hide(600, "easeInElastic");
+            });
 
-    $(".dropdown-toggle").click(function () { /*dropdown toggling*/
-        $(".dropdown-menu").toggle(600);
-    });
-
-    $(".dropdown-menu").click(function () { /*dropdown hiding*/
-        $(".dropdown-menu").hide(600, "easeInElastic");
-    });
-
-    //Collapses the navigation bar when scrolled down
-    $(document).scroll(scrollFucntion);
+    /*Collapses the navigation bar when scrolled down*/
+        $(document).scroll(Navigation_collaps);
 };
-
-var scrollFucntion = function(){
+/*Navigation bar collapse function*/
+var Navigation_collaps = function(){
     var current_scroll_top = $(document).scrollTop();
     if (current_scroll_top >= 50) {
         $(".transperent_nav").removeClass("transperent_nav", 200);
@@ -29,6 +21,15 @@ var scrollFucntion = function(){
         $("#Main_nav").addClass("transperent_nav", 200);
     }
 }
-
+/*Highlight the selected item*/
+var Navnar_highlight = function () {
+    var current = $(".activate");
+    if (current !== null) {
+        current.removeClass("activate", 600);
+    }
+    if (!$(this).hasClass("dropdown")) {
+        $(this).addClass("activate", 600);
+    }
+}
 
 $(document).ready(main);
