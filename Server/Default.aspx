0602b0c473b89a024820e3a6d3338df54e23b434
BomB1La <ofirp74@gmail.com>
2014-12-11 15:29:18 +0200

    up database

---
+++ Server/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Pages_Default" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <%="Email: " + email %>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Server/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Pages_Default : System.Web.UI.Page
+{
+    public string email;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Form["login"] != null) {
+            email = Request.Form["email"];
+        } else {
+            email = "Error";
+        }
+    }
+
+    public bool checkInformation(string email, string password) {
+
+        return true;
+    }
+}
